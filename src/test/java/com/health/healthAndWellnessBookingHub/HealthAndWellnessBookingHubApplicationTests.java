d1636b2e8ce6a824f6d00ef8368c2d78634b839c
Atul Kumar <rockatulmohit@gmail.com>
2025-02-23 09:41:35 +0530

    adding intial file

---
+++ src/test/java/com/health/healthAndWellnessBookingHub/HealthAndWellnessBookingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.healthAndWellnessBookingHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAndWellnessBookingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
